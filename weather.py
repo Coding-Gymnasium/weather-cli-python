d44e7866431a2b0d338a5d21cf7667217459baa8
Nico Rithner <nicorithner@gmail.com>
2022-08-24 17:14:27 +0200

    Build url with user input and api key

--- weather.py
+++ weather.py
@@ -1,5 +1,8 @@
 import argparse
 from configparser import ConfigParser
+from urllib import parse
+
+BASE_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 
 def read_user_cli_args():
@@ -26,6 +29,27 @@ def read_user_cli_args():
     return parser.parse_args()
 
 
+def build_weather_query(city_input, imperial=False):
+    """Builds the URL for an API request to OpenWeather's weather API.
+
+    Args:
+        city_input (List[str]): Name of a city as collected by argparse
+        imperial (bool): Whether or not to use imperial units for temperature
+
+    Returns:
+        str: URL formatted for a call to OpenWeather's city name endpoint
+    """
+    api_key = _get_api_key()
+    city_name = " ".join(city_input)
+    url_encoded_city_name = parse.quote_plus(city_name)
+    units = "imperial" if imperial else "metric"
+    url = (
+        f"{BASE_WEATHER_API_URL}?q={url_encoded_city_name}"
+        f"&units={units}&appid={api_key}"
+    )
+    return url
+
+
 def _get_api_key():
     """
     Fetch the API key from your configuration file
@@ -40,4 +64,5 @@ def _get_api_key():
 
 if __name__ == "__main__":
     user_args = read_user_cli_args()
-    print(user_args.city, user_args.imperial)
+    query_url = build_weather_query(user_args.city, user_args.imperial)
+    print(query_url)
