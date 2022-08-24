48098872af60912c442012cc9ce33c07ba72c607
Nico Rithner <nicorithner@gmail.com>
2022-08-24 16:36:15 +0200

    Set up argparse to receive and process information
    that your users will pass through the
    CLI

--- weather.py
+++ weather.py
@@ -1,4 +1,29 @@
+import argparse
 from configparser import ConfigParser
+
+
+def read_user_cli_args():
+    """Handles the CLI user interactions
+
+    Returns:
+        argparse.Namespace: Populated mamespace object
+    """
+    parser = argparse.ArgumentParser(
+        description="gets weather and temperature information for a city"
+    )
+    parser.add_argument(
+        "city",
+        nargs="+",
+        type=str,
+        help="enter the city name"
+    )
+    parser.add_argument(
+        "-i",
+        "--imperial",
+        action="store_true",
+        help="display the temperature in imperial units",
+    )
+    return parser.parse_args()
 
 
 def _get_api_key():
@@ -11,3 +36,8 @@ def _get_api_key():
     config = ConfigParser()
     config.read("secrets.ini")
     return config["openweather"]["api_key"]
+
+
+if __name__ == "__main__":
+    user_args = read_user_cli_args()
+    print(user_args.city, user_args.imperial)
